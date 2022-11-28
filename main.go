478bfa6ff1055c4d6bff5fd5b464972e2ccabfbb
Arturo <72474684+adev-exe@users.noreply.github.com>
2022-11-28 17:55:08 -0500

    first

---
+++ main.go
@@ -0,0 +1,28 @@
+
+package main
+
+import "fmt"
+
+// halveEvensImperative
+
+func halveEvensImperative(lst []int){
+	var new_lst[]int
+	// for i := 0; i < len(lst); i++ {
+	// 	var new_lst[]
+
+    //     fmt.Println(lst[i])
+	// 	if % 2 == 0{
+	// 		new_lst.add[i] = lst[i]/2
+	// 	}
+    // }
+
+	return new_lst
+}
+
+func main() {
+
+//   fmt.Println("Hell ")
+  fmt.Println([1,2,3,4,5,6,7])
+
+
+}
